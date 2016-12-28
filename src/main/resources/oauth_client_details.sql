867330d5c31e583b517c0931a2a135a4efb1939e
王兵 <wangbing01@houbank.com>
2016-12-28 19:19:28 +0800

    初始化，立项

---
+++ src/main/resources/oauth_client_details.sql
@@ -0,0 +1,2 @@
+INSERT INTO `oauth_client_details` VALUES ('clientapp', 'restservice', '123456', 'read,write', 'password,refresh_token', NULL, 'USER', NULL, NULL, NULL, NULL);
+INSERT INTO `oauth_client_details` VALUES ('mobile', 'restservice', '123456', 'read,write', 'password,refresh_token', NULL, 'ADMIN', NULL, NULL, NULL, NULL);
